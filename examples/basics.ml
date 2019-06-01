0f78e0113860c1e85eaf5e7190b496723b12d609
laurent <laurent.mazare@gmail.com>
2019-06-01 20:45:25 +0100

    Cosmetic change to the example.

--- examples/basics.ml
+++ examples/basics.ml
@@ -1,4 +1,5 @@
 let () =
-  Stdio.printf "Hello World!\n%!";
   let arr = Arrow_core.Wrapper.C.null_array_new (Int64.of_int 42) in
+  let len = Arrow_core.Wrapper.C.array_get_length arr in
+  Stdio.printf "Hello World! %d\n%!" (Int64.to_int len);
   Arrow_core.Wrapper.C.object_unref arr
--- src/stubs/arrow_bindings.ml
+++ src/stubs/arrow_bindings.ml
@@ -6,7 +6,7 @@ module C (F : Cstubs.FOREIGN) = struct
   type t = unit ptr
 
   let t : t typ = ptr void
-  let array_get_length = foreign "garrow_array_get_length" (t @-> returning void)
+  let array_get_length = foreign "garrow_array_get_length" (t @-> returning int64_t)
   let null_array_new = foreign "garrow_null_array_new" (int64_t @-> returning t)
   let object_unref = foreign "g_object_unref" (t @-> returning void)
 end
