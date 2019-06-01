bcfaf7e4d8aebba767287f462eb94ebf5981104b
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-01 22:56:48 +0100

    start playing with gen.

--- gen/gen.py
+++ gen/gen.py
@@ -1,3 +1,6 @@
+import os
+os.environ['GI_TYPELIB_PATH'] = 'gen'
+
 import xml.etree.ElementTree as ET
 tree = ET.parse('gen/Arrow-1.0.gir')
 root = tree.getroot()
@@ -9,8 +12,18 @@ import gi
 gi.require_version('Arrow', '1.0')
 from gi.repository import Arrow
 rep = gi.Repository.get_default()
-o = rep.get_infos('Arrow')
-c = o[0].get_methods()
-count = c[1]
-rt = count.get_return_type()
-print(rt.get_tag_as_string())
+
+for elem in rep.get_infos('Arrow'):
+  if not isinstance(elem, gi._gi.ObjectInfo): continue
+  print('>>', elem, elem.get_name(), elem.get_type_name())
+  for method in elem.get_methods():
+    can_throw = method.can_throw_gerror()
+    args = method.get_arguments()
+    for arg in args:
+      is_opt = arg.is_optional()
+      arg_type = arg.get_type()
+      t = arg_type.get_tag_as_string()
+      i = arg_type.get_interface()
+      # if == 'interface' => t.get_interface()
+    rt = method.get_return_type()
+    print('  ', method.get_name(), method.get_symbol(), len(args), rt.get_tag_as_string(), rt.get_interface())
