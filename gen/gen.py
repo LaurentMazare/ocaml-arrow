63c673acedc99d0e357194ee7f4be7e90f5fac7c
laurent <laurent.mazare@gmail.com>
2019-06-01 22:21:03 +0100

    Tweak generator.

--- gen/gen.py
+++ gen/gen.py
@@ -3,3 +3,14 @@ tree = ET.parse('gen/Arrow-1.0.gir')
 root = tree.getroot()
 for child in root:
   print(child.tag, child.attrib)
+
+# set GI_TYPELIB_PATH
+import gi
+gi.require_version('Arrow', '1.0')
+from gi.repository import Arrow
+rep = gi.Repository.get_default()
+o = rep.get_infos('Arrow')
+c = o[0].get_methods()
+count = c[1]
+rt = count.get_return_type()
+print(rt.get_tag_as_string())
