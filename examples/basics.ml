7de96913617d917e5ba7ba5690947d43cd05638f
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-02 16:22:16 +0100

    Fix example.

--- examples/basics.ml
+++ examples/basics.ml
@@ -1,19 +1,23 @@
+open Base
 module C = Arrow_core.Wrapper
 
 let hello_world () =
-  let arr = C.NullArray.new_ (Int64.of_int 42) in
+  let arr = C.NullArray.new_ (Int64.of_int_exn 42) in
   let len = C.Array.get_length arr in
-  Stdio.printf "Hello World! %d\n%!" (Int64.to_int len)
+  Stdio.printf "Hello World! %d\n%!" (Int64.to_int_exn len)
 
 (* Similar to arrow-glib/example/build.c *)
 let build () =
   let builder = C.Int32ArrayBuilder.new_ () in
   for i = 0 to 10 do
-    let ok = C.Int32ArrayBuilder.append_value builder (Int32.of_int i) in
+    let ok = C.Int32ArrayBuilder.append_value builder (Int32.of_int_exn i) in
     assert ok
   done;
   let array = C.ArrayBuilder.finish builder in
-  Stdio.printf "%s\n%!" (C.Array.to_string array)
+  Stdio.printf "%s\n%!" (C.Array.to_string array);
+  let array = Option.value_exn (C.Int32Array.of_gobject array) in
+  let v = C.Int32Array.get_value array (Int64.of_int 3) in
+  Stdio.printf "%d\n%!" (Int32.to_int_exn v)
 
 let read () =
   let input_stream = C.MemoryMappedInputStream.new_ "/tmp/a.csv" in
