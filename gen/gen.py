88e3f1e0ccd984a0f4562c94d30cd0a8a1d2c9cc
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-02 06:50:08 +0100

    Flesh out some of the code generator.

--- gen/gen.py
+++ gen/gen.py
@@ -1,5 +1,15 @@
 import os
+import re
 os.environ['GI_TYPELIB_PATH'] = 'gen'
+
+type_names = {
+    'array': 'array_',
+}
+
+def snake_case(name):
+  s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+  res = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+  return type_names.get(res, res)
 
 import xml.etree.ElementTree as ET
 tree = ET.parse('gen/Arrow-1.0.gir')
@@ -7,16 +17,19 @@ root = tree.getroot()
 for child in root:
   print(child.tag, child.attrib)
 
-# set GI_TYPELIB_PATH
 import gi
 gi.require_version('Arrow', '1.0')
 from gi.repository import Arrow
 rep = gi.Repository.get_default()
 
-for elem in rep.get_infos('Arrow'):
-  if not isinstance(elem, gi._gi.ObjectInfo): continue
-  print('>>', elem, elem.get_name(), elem.get_type_name())
-  for method in elem.get_methods():
+def handle_object_info(oinfo, fobj):
+  oname = oinfo.get_name()
+  # ctype: oinfo.get_type_name()
+  fobj.write('  module %s = struct\n' % oname)
+  fobj.write('    type t = %s\n' % snake_case(oname))
+  fobj.write('  end\n\n')
+
+  for method in oinfo.get_methods():
     can_throw = method.can_throw_gerror()
     args = method.get_arguments()
     for arg in args:
@@ -26,4 +39,34 @@ for elem in rep.get_infos('Arrow'):
       i = arg_type.get_interface()
       # if == 'interface' => t.get_interface()
     rt = method.get_return_type()
-    print('  ', method.get_name(), method.get_symbol(), len(args), rt.get_tag_as_string(), rt.get_interface())
+
+  if oname.startswith('Array'):
+    print('>>', oinfo, oinfo.get_name(), oinfo.get_type_name())
+    for method in oinfo.get_methods():
+      can_throw = method.can_throw_gerror()
+      args = method.get_arguments()
+      for arg in args:
+        is_opt = arg.is_optional()
+        arg_type = arg.get_type()
+        t = arg_type.get_tag_as_string()
+        i = arg_type.get_interface()
+        # if == 'interface' => t.get_interface()
+      rt = method.get_return_type()
+      print('  ', method.get_name(), method.get_symbol(), len(args), rt.get_tag_as_string(), rt.get_interface())
+
+with open('src/stubs/stubs_generated.ml', 'w') as fobj:
+  fobj.write('open Ctypes\n')
+  fobj.write('module C (F : Cstubs.FOREIGN) = struct\n')
+  fobj.write('  open F\n')
+
+  for info in rep.get_infos('Arrow'):
+    if isinstance(info, gi._gi.ObjectInfo):
+      type_name = snake_case(info.get_name())
+      fobj.write('  type %s = unit ptr\n' % type_name)
+      fobj.write('  let %s : %s typ = ptr void\n' % (type_name, type_name))
+
+  fobj.write('\n')
+  for info in rep.get_infos('Arrow'):
+    if isinstance(info, gi._gi.ObjectInfo):
+      handle_object_info(info, fobj)
+  fobj.write('end')
