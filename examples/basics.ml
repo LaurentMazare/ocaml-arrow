1d61e2cb28a1f533e87a6e51401df5e5a678ec13
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-02 10:34:11 +0100

    Start generating type safe wrappers.

--- examples/basics.ml
+++ examples/basics.ml
@@ -1,5 +1,17 @@
-let () =
-  let arr = Arrow_core.Wrapper.C.null_array_new (Int64.of_int 42) in
-  let len = Arrow_core.Wrapper.C.array_get_length arr in
+module C = Arrow_core.Wrapper.C
+
+let hello_world () =
+  let arr = C.NullArray.new_ (Int64.of_int 42) in
+  let len = C.Array.get_length arr in
   Stdio.printf "Hello World! %d\n%!" (Int64.to_int len);
-  Arrow_core.Wrapper.C.object_unref arr
+  C.object_unref arr
+
+(* Similar to arrow-glib/example/build.c *)
+let build () =
+  let builder = C.Int32ArrayBuilder.new_ () in
+  (* let array = C.ArrayBuilder.finish builder in *)
+  ignore (builder, ())
+
+let () =
+  hello_world ();
+  build ()
--- src/stubs/arrow_bindings.ml
+++ src/stubs/arrow_bindings.ml
@@ -3,13 +3,11 @@ open Ctypes
 module C (F : Cstubs.FOREIGN) = struct
   open F
 
-  type t = unit ptr
+  type gobject = unit ptr
 
-  let t : t typ = ptr void
-  let array_get_length = foreign "garrow_array_get_length" (t @-> returning int64_t)
-  let _array_builder = foreign "GARROW_ARRAY_BUILDER" (t @-> returning t)
-  let null_array_new = foreign "garrow_null_array_new" (int64_t @-> returning t)
-  let object_unref = foreign "g_object_unref" (t @-> returning void)
+  let gobject : gobject typ = ptr void
+  let _array_builder = foreign "GARROW_ARRAY_BUILDER" (gobject @-> returning gobject)
+  let object_unref = foreign "g_object_unref" (gobject @-> returning void)
 
   include Stubs_generated.C (F)
 end
