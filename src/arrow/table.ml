6c83097f1d91cbfea6d9659bc5031c06eca5e45b
Laurent <laurent.mazare@gmail.com>
2019-08-20 18:48:07 -0400

    Read and write feather files.

--- src/arrow/table.ml
+++ src/arrow/table.ml
@@ -8,6 +8,17 @@ let read_csv filename =
   let csv_reader = W.CSVReader.new_ input_stream in
   W.CSVReader.read csv_reader
 
+let read_feather filename =
+  let input_stream = W.MemoryMappedInputStream.new_ filename in
+  let csv_reader = W.FeatherFileReader.new_ input_stream in
+  W.FeatherFileReader.read csv_reader
+
+let write_feather t ?(append = false) filename =
+  let input_stream = W.FileOutputStream.new_ filename append in
+  let csv_reader = W.FeatherFileWriter.new_ input_stream in
+  let ok = W.FeatherFileWriter.write csv_reader t in
+  if not ok then Printf.failwithf "writing %s failed" filename ()
+
 let to_string = W.Table.to_string
 let num_columns t = W.Table.get_n_columns t |> Unsigned.UInt32.to_int
 let num_rows t = W.Table.get_n_rows t |> Unsigned.UInt64.to_int
--- src/arrow/table.mli
+++ src/arrow/table.mli
@@ -1,7 +1,30 @@
+(** An arrow table: a table has some named columns for which
+    each row has a value.
+*)
 type t
 
+(** [read_csv filename] reads a csv file as a table. *)
 val read_csv : string -> t
+
+(** [read_feather filename] reads a feather file as a table. *)
+val read_feather : string -> t
+
+(** [write_feather t ?append filename] writes [t] to a file.
+    If [append] is true, the content is appended at the end of the
+    file, the default for [append] is [false].
+*)
+val write_feather : t -> ?append:bool -> string -> unit
+
+(** [to_string t] returns a pretty-printing of table [t]. *)
 val to_string : t -> string
+
+(** [num_columns t] returns the number of columns for table [t]. *)
 val num_columns : t -> int
+
+(** [num_rows t] returns the number of rows in table [t]. *)
 val num_rows : t -> int
+
+(** [column_names t] returns the list of columns defined in table [t].
+    The number of element returned is equal to [num_columns t].
+*)
 val column_names : t -> string list
