e116a720231ae7aa10165c04591eb8bde0c22687
laurent <laurent.mazare@gmail.com>
2020-08-16 15:57:52 +0100

    Support more file formats.

--- c_api/wrapper/file_reader.ml
+++ c_api/wrapper/file_reader.ml
@@ -1,13 +1,23 @@
 open! Base
 
-let is_feather = String.is_suffix ~suffix:".feather"
+let unknown_suffix filename =
+  Printf.failwithf
+    "cannot infer the file format from suffix %s (try using .parquet as a suffix)"
+    filename
+    ()
 
 let schema filename =
-  if is_feather filename
-  then Wrapper.Feather_reader.schema filename
-  else Wrapper.Parquet_reader.schema filename
+  match String.rsplit2 filename ~on:'.' with
+  | Some (_, "csv") -> Wrapper.Table.read_csv filename |> Wrapper.Table.schema
+  | Some (_, "json") -> Wrapper.Table.read_json filename |> Wrapper.Table.schema
+  | Some (_, "feather") -> Wrapper.Feather_reader.schema filename
+  | Some (_, "parquet") -> Wrapper.Parquet_reader.schema filename
+  | Some _ | None -> unknown_suffix filename
 
 let table ?column_idxs filename =
-  if is_feather filename
-  then Wrapper.Feather_reader.table ?column_idxs filename
-  else Wrapper.Parquet_reader.table ?column_idxs filename
+  match String.rsplit2 filename ~on:'.' with
+  | Some (_, "csv") -> Wrapper.Table.read_csv filename
+  | Some (_, "json") -> Wrapper.Table.read_json filename
+  | Some (_, "feather") -> Wrapper.Feather_reader.table ?column_idxs filename
+  | Some (_, "parquet") -> Wrapper.Parquet_reader.table ?column_idxs filename
+  | Some _ | None -> unknown_suffix filename
